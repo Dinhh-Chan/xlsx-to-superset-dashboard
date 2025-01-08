cf6b2f1d8bdffabca71c974b88de275d02b6e9f3
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-01-08 15:12:20 +0700

    update chart

--- app/models/linechart.py
+++ app/models/linechart.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, validator  
 from typing import List, Optional 
 import json 
-class NumberChartRequest(BaseModel):
+class LineChartRequest(BaseModel):
     cache_timeout: int = Field(..., example=0)
     certification_details: Optional[str] = Field("", example="Chi tiết chứng nhận")
     certified_by: Optional[str] = Field("", example="Người chứng nhận")
@@ -13,11 +13,11 @@ class NumberChartRequest(BaseModel):
     external_url: Optional[str] = Field("", example="http://example.com")
     is_managed_externally: bool = Field(..., example=False)
     owners: List[int] = Field(..., example=[1])
-    params: str = Field(..., example="{\"viz_type\":\"big_number_total\",\"metric\":{\"aggregate\":\"COUNT_DISTINCT\",\"column\":{\"advanced_data_type\":null,\"certification_details\":null,\"certified_by\":null,\"column_name\":\"Mã ngành\",\"description\":null,\"expression\":null,\"filterable\":true,\"groupby\":true,\"id\":1861,\"is_certified\":false,\"is_dttm\":false,\"python_date_format\":null,\"type\":\"STRING\",\"type_generic\":1,\"verbose_name\":null,\"warning_markdown\":null},\"datasourceWarning\":false,\"expressionType\":\"SIMPLE\",\"hasCustomLabel\":false,\"label\":\"COUNT_DISTINCT(Mã ngành)\",\"optionName\":\"metric_dk97n1u6zud_2997om4xj6s\",\"sqlExpression\":null},\"adhoc_filters\":[],\"header_font_size\":0.4,\"subheader_font_size\":0.15,\"y_axis_format\":\"SMART_NUMBER\",\"time_format\":\"smart_date\",\"extra_form_data\":{}}")
+    params: str = Field(..., example="{\"viz_type\":\"echarts_timeseries_line\",\"x_axis\":\"Trạng thái học\",\"time_grain_sqla\":\"P1D\",\"xAxisForceCategorical\":true,\"x_axis_sort\":\"count\",\"x_axis_sort_asc\":true,\"x_axis_sort_series\":\"name\",\"x_axis_sort_series_ascending\":true,\"metrics\":[\"count\"],\"groupby\":[],\"adhoc_filters\":[],\"order_desc\":true,\"row_limit\":10000,\"truncate_metric\":true,\"show_empty_columns\":true,\"comparison_type\":\"values\",\"annotation_layers\":[],\"forecastPeriods\":10,\"forecastInterval\":0.8,\"x_axis_title_margin\":15,\"y_axis_title_margin\":15,\"y_axis_title_position\":\"Left\",\"sort_series_type\":\"sum\",\"color_scheme\":\"supersetColors\",\"seriesType\":\"line\",\"only_total\":true,\"opacity\":0.2,\"markerSize\":6,\"show_legend\":true,\"legendType\":\"scroll\",\"legendOrientation\":\"top\",\"x_axis_time_format\":\"smart_date\",\"rich_tooltip\":true,\"tooltipTimeFormat\":\"smart_date\",\"y_axis_format\":\"SMART_NUMBER\",\"truncateXAxis\":true,\"y_axis_bounds\":[null,null],\"extra_form_data\":{}}")
     query_context: Optional[str] = Field("", example="Context info")
     query_context_generation: bool = Field(..., example=False)
     slice_name: str = Field(..., example="Thống kê tình trạng học tập của sinh viên")
-    viz_type: str = Field(..., example="big_number_total")
+    viz_type: str = Field(..., example="echarts_timeseries_line")
 
     @validator('params')
     def validate_params(cls, v):
